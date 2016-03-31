7c920b953d3eee816e6053c229a9bcfaef390378
ResidentZealoT <resident@hotmail.co.za>
2016-03-31 02:59:53 +0200

    Added Player Ship and Enemy Ship

---
+++ Unity2DSpaceShooter/Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGun : MonoBehaviour {
+
+	public GameObject EnemyBulletGO;
+	// Use this for initialization
+	void Start () {
+
+		Invoke ("Fire", 1f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Fire()
+	{
+		GameObject playerShip = GameObject.Find ("Player1GO");
+
+		if (playerShip != null) {
+			GameObject bullet = (GameObject)Instantiate (EnemyBulletGO);
+
+			bullet.transform.position = transform.position;
+
+			Vector2 direction = playerShip.transform.position - bullet.transform.position;
+
+			bullet.GetComponent<EnemyBullets>().SetDirection (direction);
+		}
+	}
+}
---
+++ Unity2DSpaceShooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour 
+{
+
+	public GameObject EnemyGO;
+
+	float SpawnRate = 5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke ("SpawnEnemy", SpawnRate);
+
+		InvokeRepeating ("IncreaseDifficulty", 0f, 30f);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void SpawnEnemy ()
+	{
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+		GameObject EnemyInstance = (GameObject)Instantiate (EnemyGO);
+		EnemyInstance.transform.position = new Vector2 (Random.Range (min.x, max.x), max.y);
+
+		ScheduleSpawn ();
+	}
+
+	void ScheduleSpawn()
+	{
+		float spawn;
+
+		if (SpawnRate > 1f) {
+			spawn = Random.Range (1f, SpawnRate);
+		} else
+			spawn = 1f;
+
+		Invoke ("SpawnEnemy", spawn);
+	}
+
+	void IncreaseDIfficulty()
+	{
+		if (SpawnRate > 1f)
+			SpawnRate--;
+		if (SpawnRate == 1f)
+			CancelInvoke ("IncreaseDifficulty");
+			
+	}
+
+}
